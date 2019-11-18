3b2d33bb3c609322ae56c5b767fdbf538b7384f8
Federico Bergamo <bergamo.fed@gmail.com>
2019-11-18 11:09:41 +0100

    game hub

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bag.Scripts.Generic;
+
+public class GameData
+{
+	const string fileName = "gameData";
+	public static GameData data;
+	public static int IntData { get { return data.intData; } }
+
+	[SerializeField] string debug = "fail";
+	[SerializeField] public int intData;
+
+	public static void Init()
+	{
+		if(FileLoader.JsonExists("gameData"))
+		{
+			data = JsonUtility.FromJson<GameData>(FileLoader.LoadJson(fileName));
+			Debug.Log("[GAME DATA] Game data loaded from database json file '" + fileName + "'. Load result is '" + data.debug + "'.");
+		}
+		else
+			data = new GameData();
+	}
+}
