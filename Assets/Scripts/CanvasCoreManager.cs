c3d3396184af240425429a45efebb74f617abbfb
Federico Bergamo <bergamo.fed@gmail.com>
2019-11-18 11:23:18 +0100

    FIX GAMEHUB

---
+++ Assets/Scripts/CanvasCoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bag.Mobile.UiLite
+{
+	public class CanvasCoreManager : CanvasManager
+	{
+		new public static CanvasCoreManager Singleton { get; set; }
+
+		public event Action restartGame;
+
+		public void InitGameGraphics()
+		{
+			Debug.Log("[CANVAS CORE MANAGER] Init game graphics.");
+
+			//goalBarHolder.SetActive(!Game.LevelIsSandbox);
+
+			// init game canvas graphics here
+			//Game.Player.onTimeChange += t => { timeTxt.text = t > 60 ? TimerStrings.GetTimerMinutes(t, msLength: 1) : TimerStrings.GetTimerSeconds(t, msLength: 1); };
+			//Game.Player.onScore += (f, s, n) =>
+			//Game.Player.onScore += (f, s, n) => { ScoreUpdateUI(s, n); };
+		}
+	}
+}
